fe208b8ef79c48ed66372babe6f0d6dec8580efc
p1Gaur <pawan.gaur@accoliteindia.com>
2018-07-13 13:15:42 +0530

    commit

---
+++ src/main/java/com/au/entities/EventItemDetails.java
@@ -0,0 +1,89 @@
+package com.au.entities;
+
+import javax.persistence.Column;
+
+public class EventItemDetails {
+
+	private int itemId;
+	
+	private String itemName;
+
+	private String itemType;
+
+	private String itemDescription;
+
+	private Double itemPrice;
+
+	private String itemImages;
+
+	private int eventId;
+	
+	private String eventName;
+
+	public int getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(int itemId) {
+		this.itemId = itemId;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public String getItemType() {
+		return itemType;
+	}
+
+	public void setItemType(String itemType) {
+		this.itemType = itemType;
+	}
+
+	public String getItemDescription() {
+		return itemDescription;
+	}
+
+	public void setItemDescription(String itemDescription) {
+		this.itemDescription = itemDescription;
+	}
+
+	public Double getItemPrice() {
+		return itemPrice;
+	}
+
+	public void setItemPrice(Double itemPrice) {
+		this.itemPrice = itemPrice;
+	}
+
+	public String getItemImages() {
+		return itemImages;
+	}
+
+	public void setItemImages(String itemImages) {
+		this.itemImages = itemImages;
+	}
+
+	public int getEventId() {
+		return eventId;
+	}
+
+	public void setEventId(int eventId) {
+		this.eventId = eventId;
+	}
+
+	public String getEventName() {
+		return eventName;
+	}
+
+	public void setEventName(String eventName) {
+		this.eventName = eventName;
+	}
+	
+	
+
+}
